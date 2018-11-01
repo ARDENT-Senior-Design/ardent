48019d5fd56d7840d2455d293b99f693baac9ebb
Nathan Boyd <nathan.boyd.481@my.csun.edu>
2018-10-31 17:56:04 -0700

    Updated Inverse Kinematics

--- include/kinematics.h
+++ include/kinematics.h
@@ -1,35 +1,45 @@
-#ifndef INVERSE_KINEMATICS_H_
-#define INVERSE_KINEMATICS_H_
+#ifndef KINEMATICS_H_
+#define KINEMATICS_H_
 
-#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+
 namespace ardent{
 
-    enum ArdentJointID {COXA, FEMUR, TIBIA, JointCount};
+    enum ArdentJointID {COXA, FEMUR, TIBIA, EE, JointCount};
 
-    class ArdentInverseKinematics{
+    class ArdentKinematics{
         public:
             using Vector3d = Eigen::Vector3d;
             /**
              * @brief Default initialize leg lengths with values
              */
-            ArdentInverseKinematics() = default;
-            virtual ~ArdentInverseKinematics() = default;
+            ArdentKinematics() = default;
+            virtual ~ArdentKinematics() = default;
 
             /**
-             * @brief Return the joint angles to reach cartesian position
+             * @brief Return the joint angles to reach cartesian position in a vector array. In order, the vector array of each angle is: Coxa, femur, tibia
              * @param ee_pos End Effector position xyz relative to frame relative to j_c1_rf
              */ 
-            Vector3d GetJointAngles(Vector3d& ee_poss);
+            Vector3d GetJointAngles(Vector3d& ee_pos);
             
+            /**
+             * @brief Return the cartesian position
+             * @param id Joint id for the position
+             */
+            Vector3d GetJointPosition(ArdentJointID id);
+
+
             /**
              * @brief Restirct the joint angles to stay within reasonable limits
              * @param q[in/out] Current joint angle that will get adapted if out of range
              * @param joint Which joint (COXA, FEMUR, TIBIA) the joint represents
              */
-            void ForceJointConstraints(double& q, ArdentJointID);
+            void ForceJointConstraints(double& q, ArdentJointID id);
+
+
         
         private:
-            Vector3d offset_z = Vector3d(0.0,0.0,0.35); //Distance from center of body to coxa
+            Vector3d offset_z = Vector3d(1.53,0.85,0.0); //Distance from center of body to coxa
             double coxa_length = 0.1; // link length from first motor (j_coxa) to second motor (j_femur)
             double femur_length = 0.25; // link length from second motor (j_femur) to third joint (j_tibia)
             double tibia_length = 0.25; //link length from third motor (j_tibia) to the end effector
--- src/inverse_kinematics.cpp
+++ src/inverse_kinematics.cpp
@@ -1,1 +1,26 @@
-#include "../include/inverse_kinematics.h"
+#include <../include/inverse_kinematics.h>
+
+
+namespace ardent{
+    
+        ArdentKinematics::Vector3d ArdentKinematics::GetJointAngles(Vector3d& ee_pos)
+        {
+            Eigen::Matrix4d h;  //transformation matrix for the joint
+            Vector3d qr = ee_pos;
+            
+            // Transformation matrix for all joints
+            
+            //Enforce Limits
+        }
+
+        ArdentKinematics::Vector3d ArdentKinematics::GetJointPosition(ArdentJointID& q)
+        {
+
+        }
+
+        ArdentKinematics::Vector3d ArdentKinematics::ForceJointConstraints(double& qq, ArdentJointID id)
+        {
+            
+        }
+
+}
