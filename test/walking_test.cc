4f240fbdf4911bf722fe87054c3e46a57e5cb9ad
Nathan Boyd <nathan.boyd.481@my.csun.edu>
2018-11-21 11:45:12 -0800

    Edited controller to be based on trajectory instead of position

--- test/walking_test.cc
+++ test/walking_test.cc
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ardent::ArdentRobot ardent; 
     //test
-    
+    // blah
     Eigen::Vector3d command1 = Eigen::Vector3d(0.35,0.0,-0.25); //relative to the center of the body
     Eigen::Vector3d command2 = Eigen::Vector3d(0.2,0.0,0);
 
